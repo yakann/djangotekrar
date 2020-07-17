a7902e57de9583263d576c73fad6da407d5b4b11
yakann <42899073+yakann@users.noreply.github.com>
2020-07-17 14:00:22 +0300

    Generic Views

--- altProje/urls.py
+++ altProje/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import cari_list, cari_detail, CariListesi, CariDetails
+from .views import cari_list, cari_detail, CariListesi, CariDetails, GenericCariListesi
 
 
 urlpatterns = [
     #path('altproje/', views.cari_list),
     path('altproje/', CariListesi.as_view()),
     #path('altproje/<uuid:pk>/', cari_detail),
-    path('altproje/<uuid:pk>/', CariDetails.as_view()),
+    path('altproje/<uuid:id>/', CariDetails.as_view()),
+    path('altproje/generic/<uuid:id>/', GenericCariListesi.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- altProje/views.py
+++ altProje/views.py
@@ -5,8 +5,6 @@ from django.http import HttpResponse
 # Rest Api
 # from rest_framework.views import APIView
 
- 
-
 from django.views.decorators.csrf import csrf_exempt
 
 from .serializers import CariSerializer, CariAdresSerializer
@@ -17,6 +15,30 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
+from rest_framework import generics
+from rest_framework import mixins
+
+#GENERİC VIEWS
+
+class GenericCariListesi(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = CariSerializer
+    queryset = Cari.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        return self.list(request)
+    
+    def post(self, request, id=None):
+        return self.create(request)
+    
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
 
 #CLASS BASED VIEW
 class CariListesi(APIView):
